f139a14fbf6e968974785409f01ce57417fa798f
Deividas Karžinauskas <deividas.karzinauskas@gmail.com>
2024-08-22 15:45:11 +0300

    Nx setup (#2)
    * Try nx setup
    
    * Test
    
    * Test 2
    
    * Test 3
    
    * Test 4
    
    * Test 5
    
    * Test 6
    
    * Test 7
    
    * Test 8
    
    * Test 9
    
    * Test 10
    
    * Test 11
    
    * Test 12
    
    * Naming
    
    * Test 13
    
    * Test 14
    
    * Test 15
    
    * Fix test
    
    * Setup go
    
    * Fix golangci root
    
    * Fix golangci root 2
    
    * Simplify
    
    * Separate lint
    
    * Test 21
    
    * Increase timeout
    
    * Fix lint
    
    * Add another check
    
    * setup-node@v4
    
    * Update go deps cache path
    
    * Add go deps cache

--- capabilities/kvstore/target/target.go
+++ capabilities/kvstore/target/target.go
@@ -73,30 +73,30 @@ func evaluate(rawRequest capabilities.CapabilityRequest) (r Request, err error) 
 }
 
 func (c *capability) Execute(ctx context.Context, rawRequest capabilities.CapabilityRequest) (<-chan capabilities.CapabilityResponse, error) {
-	c.logger.Debugf("Executing", "WorkflowID", rawRequest.Metadata.WorkflowID, "WorkflowExecutionID", rawRequest.Metadata.WorkflowExecutionID)
+	c.logger.Debug("Executing", "WorkflowID", rawRequest.Metadata.WorkflowID, "WorkflowExecutionID", rawRequest.Metadata.WorkflowExecutionID)
 
 	request, err := evaluate(rawRequest)
 	if err != nil {
 		return nil, fmt.Errorf("failed to decode signed report: %v", err)
 	}
 
-	c.logger.Debugf("Decoded signed report", "WorkflowID", request.Metadata.WorkflowID, "WorkflowExecutionID", request.Metadata.WorkflowExecutionID, "ReportVersion", request.Inputs.SignedReport.Version)
+	c.logger.Debug("Decoded signed report", "WorkflowID", request.Metadata.WorkflowID, "WorkflowExecutionID", request.Metadata.WorkflowExecutionID, "ReportVersion", request.Inputs.SignedReport.Version)
 
 	if err := c.store.Store(ctx, "some", []byte{1, 2, 3}); err != nil {
 		return nil, err
 	}
-	c.logger.Debugf("Value stored", "WorkflowID", rawRequest.Metadata.WorkflowID, "WorkflowExecutionID", rawRequest.Metadata.WorkflowExecutionID)
+	c.logger.Debug("Value stored", "WorkflowID", rawRequest.Metadata.WorkflowID, "WorkflowExecutionID", rawRequest.Metadata.WorkflowExecutionID)
 
 	return success(), nil
 }
 
 func (c *capability) RegisterToWorkflow(ctx context.Context, rawRequest capabilities.RegisterToWorkflowRequest) error {
-	c.logger.Debugf("Registering to workflow", "WorkflowID", rawRequest.Metadata.WorkflowID, "WorkflowExecutionID")
+	c.logger.Debug("Registering to workflow", "WorkflowID", rawRequest.Metadata.WorkflowID, "WorkflowExecutionID")
 
 	return nil
 }
 
 func (c *capability) UnregisterFromWorkflow(ctx context.Context, rawRequest capabilities.UnregisterFromWorkflowRequest) error {
-	c.logger.Debugf("Unregistering from workflow", "WorkflowID", rawRequest.Metadata.WorkflowID, "WorkflowExecutionID")
+	c.logger.Debug("Unregistering from workflow", "WorkflowID", rawRequest.Metadata.WorkflowID, "WorkflowExecutionID")
 	return nil
 }
