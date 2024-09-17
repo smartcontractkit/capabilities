59231049867f254f99e17051ff5c45b99d16b109
Ryan Tinianov <tinianov@live.com>
2024-09-17 13:53:16 -0400

    move comment and add no-op test...

--- capabilities/gen.go
+++ capabilities/gen.go
@@ -1,6 +1,5 @@
 package capabilities
 
-// Required so that the tool is available to be run in go generate below.
-import _ "github.com/smartcontractkit/chainlink-common/pkg/capabilities/cli/cmd"
+import _ "github.com/smartcontractkit/chainlink-common/pkg/capabilities/cli/cmd" // Required so that the tool is available to be run in go generate below.
 
 //go:generate go run github.com/smartcontractkit/chainlink-common/pkg/capabilities/cli/cmd/generate-types --dir $GOFILE --extra_urls https://raw.githubusercontent.com/smartcontractkit/chainlink-common/main/pkg/capabilities/consensus/ocr3/ocr3cap/ocr3cap_common-schema.json
---
+++ capabilities/gen_test.go
@@ -0,0 +1,9 @@
+package capabilities
+
+import "testing"
+
+func Test(t *testing.T) {
+	// required so lint knows the package was tested.
+	// the exit code from running tests is non-zero if there are none.
+	// CI detects that this is a go package.
+}
