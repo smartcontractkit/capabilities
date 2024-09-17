808aa605607f89d4825a65e11dce073663229ea1
Ryan Tinianov <tinianov@live.com>
2024-09-17 13:48:03 -0400

    fix lint by adding a comment

--- capabilities/gen.go
+++ capabilities/gen.go
@@ -1,5 +1,6 @@
 package capabilities
 
+// Required so that the tool is available to be run in go generate below.
 import _ "github.com/smartcontractkit/chainlink-common/pkg/capabilities/cli/cmd"
 
 //go:generate go run github.com/smartcontractkit/chainlink-common/pkg/capabilities/cli/cmd/generate-types --dir $GOFILE --extra_urls https://raw.githubusercontent.com/smartcontractkit/chainlink-common/main/pkg/capabilities/consensus/ocr3/ocr3cap/ocr3cap_common-schema.json
