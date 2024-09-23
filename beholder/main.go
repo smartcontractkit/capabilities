c8c75a6cf4c8b7ac420e52ffb2a69393fd9a2f49
Fergal Gribben <fergal.gribben@smartcontract.com>
2024-09-23 23:36:10 +0100

    WIP add a beholder capability
    Regenerate
    
    Refactor
    
    Add test coverage
    
    Clean up project.json
    
    goimports
    
    Add generate task
    
    Nx tidy
    
    Regenerate beholder capability
    
    Update tests
    
    Use proto maps
    
    Use proto marshal and unmarshal
    
    Switch to google.golang.org/protobuf/proto
    
    Clean up generated code
    
    Nx generate
    
    go mod tidy
    
    WIP update cap
    
    Bump common
    
    Use the test config to disable batch, specify attributes
    
    WIP update beholder settings
    
    Update tests
    
    Update test
    
    Add readme with nx commands
    
    Please the linter

---
+++ beholder/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/go-plugin"
+
+	"github.com/smartcontractkit/capabilities/beholder/target"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/capabilities"
+	"github.com/smartcontractkit/chainlink-common/pkg/loop"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/core"
+)
+
+const (
+	serviceName = "BeholderCapabilities"
+)
+
+type CapabilitiesService struct {
+	target capabilities.TargetCapability
+	s      *loop.Server
+}
+
+func main() {
+	s := loop.MustNewStartedServer(serviceName)
+	defer s.Stop()
+
+	s.Logger.Infof("Starting service %s", serviceName)
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: loop.StandardCapabilitiesHandshakeConfig(),
+		Plugins: map[string]plugin.Plugin{
+			loop.PluginStandardCapabilitiesName: &loop.StandardCapabilitiesLoop{
+				PluginServer: &CapabilitiesService{
+					s: s,
+				},
+				BrokerConfig: loop.BrokerConfig{Logger: s.Logger, StopCh: stopCh, GRPCOpts: s.GRPCOpts},
+			},
+		},
+		GRPCServer: s.GRPCOpts.NewServer,
+	})
+}
+
+func (cs *CapabilitiesService) Start(ctx context.Context) error {
+	return nil
+}
+
+func (cs *CapabilitiesService) Close() error {
+	return nil
+}
+
+func (cs *CapabilitiesService) Ready() error {
+	return nil
+}
+
+func (cs *CapabilitiesService) HealthReport() map[string]error {
+	return nil
+}
+
+func (cs *CapabilitiesService) Name() string {
+	return serviceName
+}
+
+func (cs *CapabilitiesService) Infos(ctx context.Context) ([]capabilities.CapabilityInfo, error) {
+	targetInfo, err := cs.target.Info(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return []capabilities.CapabilityInfo{
+		targetInfo,
+	}, nil
+}
+
+func (cs *CapabilitiesService) Initialise(
+	ctx context.Context,
+	_ string,
+	_ core.TelemetryService,
+	_ core.KeyValueStore,
+	capabilityRegistry core.CapabilitiesRegistry,
+	_ core.ErrorLog,
+	_ core.PipelineRunnerService,
+	_ core.RelayerSet,
+) error {
+	cs.s.Logger.Debugf("Initialising %s", serviceName)
+	cs.target = target.New(target.Params{
+		Logger: cs.s.Logger,
+	})
+
+	if err := capabilityRegistry.Add(ctx, cs.target); err != nil {
+		return fmt.Errorf("error when adding kv store target to the registry: %w", err)
+	}
+
+	return nil
+}
---
+++ beholder/target/target_test.go
@@ -0,0 +1,249 @@
+package target
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/protobuf/proto"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/beholder"
+	"github.com/smartcontractkit/chainlink-common/pkg/capabilities"
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+	"github.com/smartcontractkit/chainlink-common/pkg/values"
+)
+
+func TestNew(t *testing.T) {
+	t.Run("a new beholder capability is created", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+		assert.NotNil(t, c)
+	})
+}
+
+func TestCapability_Info(t *testing.T) {
+	t.Run("capability info is reported correctly", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+		info, err := c.Info(context.Background())
+		assert.NoError(t, err)
+		assert.Equal(t, "beholder-target@1.0.0", info.ID)
+		assert.Equal(t, capabilities.CapabilityType("target"), info.CapabilityType)
+		assert.Equal(t, "Emits messages through beholder", info.Description)
+		assert.Equal(t, true, info.IsLocal)
+	})
+}
+
+func TestCapability_Execute(t *testing.T) {
+	t.Run("capability executes without error", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+
+		emitter := &mockEmitter{EmitFn: func(ctx context.Context, body []byte, attrKVs ...any) error {
+			assert.Len(t, attrKVs, 4)
+
+			var valueMap values.Map
+			pbm := values.ProtoMap(&valueMap)
+			err := unmarshalFn(body, pbm)
+			assert.NoError(t, err)
+
+			rawMap := map[string]any{}
+			for k, v := range pbm.Fields {
+				rawMap[k] = v.GetStringValue()
+			}
+
+			assert.Equal(t, rawMap, map[string]any{"service": "Beholder", "component": "Unit test"})
+			return nil
+		}}
+
+		mockBeholderClient := &beholder.Client{
+			Emitter: emitter,
+		}
+
+		oldNewClientFn := newClientFn
+		newClientFn = func(cfg beholder.Config) (*beholder.Client, error) {
+			return mockBeholderClient, nil
+		}
+		defer func() {
+			newClientFn = oldNewClientFn
+		}()
+
+		payload, err := values.NewMap(map[string]any{
+			"service":   values.NewString("Beholder"),
+			"component": values.NewString("Unit test"),
+		})
+		assert.NoError(t, err)
+
+		_, err = c.Execute(context.Background(), capabilities.CapabilityRequest{
+			Inputs: &values.Map{Underlying: map[string]values.Value{
+				"payload": payload,
+			}},
+		})
+		assert.NoError(t, err)
+	})
+
+	t.Run("capability errors when inputs is nil", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+		_, err := c.Execute(context.Background(), capabilities.CapabilityRequest{
+			Inputs: nil,
+		})
+		assert.Error(t, err)
+		assert.Equal(t, "missing inputs field", err.Error())
+	})
+
+	t.Run("capability errors when payload is missing", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+		_, err := c.Execute(context.Background(), capabilities.CapabilityRequest{
+			Inputs: &values.Map{Underlying: map[string]values.Value{}}})
+		assert.Error(t, err)
+		assert.Equal(t, "missing payload", err.Error())
+	})
+
+	t.Run("capability errors when payload is nil", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+		_, err := c.Execute(context.Background(), capabilities.CapabilityRequest{
+			Inputs: &values.Map{Underlying: map[string]values.Value{
+				"payload": nil,
+			}}})
+		assert.Error(t, err)
+		assert.Equal(t, "missing payload", err.Error())
+	})
+
+	t.Run("capability errors when payload is not a map", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+		_, err := c.Execute(context.Background(), capabilities.CapabilityRequest{
+			Inputs: &values.Map{Underlying: map[string]values.Value{
+				"payload": values.NewString("test"),
+			}}})
+		assert.Error(t, err)
+		assert.Equal(t, "invalid payload", err.Error())
+	})
+
+	t.Run("capability errors when marshalling errors", func(t *testing.T) {
+		oldMarshal := marshalFn
+		marshalFn = func(v proto.Message) ([]byte, error) {
+			return nil, errors.New("boom")
+		}
+		defer func() {
+			marshalFn = oldMarshal
+		}()
+
+		payload, err := values.NewMap(map[string]any{
+			"name": values.NewString("test"),
+		})
+
+		assert.NoError(t, err)
+		c := New(Params{Logger: logger.Test(t)})
+		_, err = c.Execute(context.Background(), capabilities.CapabilityRequest{
+			Inputs: &values.Map{Underlying: map[string]values.Value{
+				"payload": payload,
+			}},
+		})
+		assert.Error(t, err)
+		assert.Equal(t, "boom", err.Error())
+	})
+
+	t.Run("capability errors when creating a new map errors", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+		newMapFunction := newMapFn
+		newMapFn = func(m map[string]any) (*values.Map, error) {
+			return nil, errors.New("new map boom")
+		}
+		defer func() {
+			newMapFn = newMapFunction
+		}()
+
+		payload, err := values.NewMap(map[string]any{
+			"name": values.NewString("test"),
+		})
+		assert.NoError(t, err)
+
+		_, err = c.Execute(context.Background(), capabilities.CapabilityRequest{
+			Inputs: &values.Map{Underlying: map[string]values.Value{
+				"payload": payload,
+			}},
+		})
+		assert.Error(t, err)
+		assert.Equal(t, "new map boom", err.Error())
+	})
+
+	t.Run("capability errors when creating the beholder client errors", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+
+		oldNewClientFn := newClientFn
+		newClientFn = func(cfg beholder.Config) (*beholder.Client, error) {
+			return nil, errors.New("new client boom")
+		}
+		defer func() {
+			newClientFn = oldNewClientFn
+		}()
+
+		payload, err := values.NewMap(map[string]any{
+			"name": values.NewString("test"),
+		})
+		assert.NoError(t, err)
+
+		_, err = c.Execute(context.Background(), capabilities.CapabilityRequest{
+			Inputs: &values.Map{Underlying: map[string]values.Value{
+				"payload": payload,
+			}},
+		})
+		assert.Error(t, err)
+		assert.Equal(t, "new client boom", err.Error())
+	})
+
+	t.Run("capability errors when emit errors", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+
+		emitter := &mockEmitter{EmitFn: func(ctx context.Context, body []byte, attrKVs ...any) error {
+			return errors.New("emit boom")
+		}}
+
+		mockBeholderClient := &beholder.Client{
+			Emitter: emitter,
+		}
+
+		oldNewClientFn := newClientFn
+		newClientFn = func(cfg beholder.Config) (*beholder.Client, error) {
+			return mockBeholderClient, nil
+		}
+		defer func() {
+			newClientFn = oldNewClientFn
+		}()
+
+		payload, err := values.NewMap(map[string]any{
+			"name": values.NewString("test"),
+		})
+		assert.NoError(t, err)
+
+		_, err = c.Execute(context.Background(), capabilities.CapabilityRequest{
+			Inputs: &values.Map{Underlying: map[string]values.Value{
+				"payload": payload,
+			}},
+		})
+		assert.Error(t, err)
+		assert.Equal(t, "emit boom", err.Error())
+	})
+}
+
+type mockEmitter struct {
+	EmitFn func(ctx context.Context, body []byte, attrKVs ...any) error
+}
+
+func (e *mockEmitter) Emit(ctx context.Context, body []byte, attrKVs ...any) error {
+	return e.EmitFn(ctx, body, attrKVs...)
+}
+
+func TestCapability_UnregisterFromWorkflow(t *testing.T) {
+	t.Run("unregister from workflow does not error", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+		err := c.UnregisterFromWorkflow(context.Background(), capabilities.UnregisterFromWorkflowRequest{})
+		assert.NoError(t, err)
+	})
+}
+
+func TestCapability_RegisterToWorkflow(t *testing.T) {
+	t.Run("register to workflow does not error", func(t *testing.T) {
+		c := New(Params{Logger: logger.Test(t)})
+		err := c.RegisterToWorkflow(context.Background(), capabilities.RegisterToWorkflowRequest{})
+		assert.NoError(t, err)
+	})
+}
