e1fc8a9bcb3cac12f4dc0040b0ee0b34267754e4
Fergal <fergal.gribben@smartcontract.com>
2024-11-12 22:09:36 +0000

    Workflow event capability (#19)
    * WIP add a beholder capability
    
    Regenerate
    
    Refactor
    
    Add test coverage
    
    Clean up project.json
    
    goimports
    
    Add generate task
    
    Nx tidy
    
    Regenerate beholder capability
    
    Update tests
    
    Use proto maps
    
    Use proto marshal and unmarshal
    
    Switch to google.golang.org/protobuf/proto
    
    Clean up generated code
    
    Nx generate
    
    go mod tidy
    
    WIP update cap
    
    Bump common
    
    Use the test config to disable batch, specify attributes
    
    WIP update beholder settings
    
    Update tests
    
    Update test
    
    Add readme with nx commands
    
    Please the linter
    
    * Use a non test config
    
    * Update build commands
    
    * Update the initialise function signature to allow for an OracleFactory
    
    * go mod tidy
    
    * Cast to values.Map and use values.ProtoMap
    
    * goimports
    
    * Add more logs
    
    * Update tests, remove logs
    
    * Please the linter
    
    * Add example payloads to the schema
    
    * Add attributes to the emitted message
    
    * Rename the beholder capability to the workflowevent capability
    
    * Fix tests
    
    * Add beholder domain and entity, TBD
    
    * Update tests
    
    * Update workflowevent/project.json
    
    Co-authored-by: Deividas Karžinauskas <deividas.karzinauskas@gmail.com>
    
    * Use the loopserver lib
    
    * Extract capability info
    
    * Update workflowevent/project.json
    
    Co-authored-by: chudilka1 <yepishevsanya@gmail.com>
    
    * Goimports
    
    * Use testutils
    
    * Goimports
    
    * go mod tidy
    
    * Rename schema json
    
    * Exclude main.go from coverage reports
    
    * Make domain and entity configurable
    
    * Use platform and values as domain and entity
    
    ---------
    
    Co-authored-by: Sri Kidambi <1702865+kidambisrinivas@users.noreply.github.com>
    Co-authored-by: Deividas Karžinauskas <deividas.karzinauskas@gmail.com>
    Co-authored-by: chudilka1 <yepishevsanya@gmail.com>

---
+++ workflowevent/target/target.go
@@ -0,0 +1,108 @@
+package target
+
+import (
+	"context"
+	"errors"
+
+	"google.golang.org/protobuf/proto"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/beholder"
+	"github.com/smartcontractkit/chainlink-common/pkg/capabilities"
+	"github.com/smartcontractkit/chainlink-common/pkg/logger"
+	"github.com/smartcontractkit/chainlink-common/pkg/values"
+	"github.com/smartcontractkit/chainlink-common/pkg/workflows/sdk"
+)
+
+var (
+	marshalFn               = proto.Marshal
+	unmarshalFn             = proto.Unmarshal
+	newClientFn             = beholder.NewClient
+	workflowEventTargetInfo = capabilities.MustNewCapabilityInfo(
+		"workflowevent-target@1.0.0",
+		capabilities.CapabilityTypeTarget,
+		"Emits messages through an OTEL client",
+	)
+)
+
+type Params struct {
+	Logger logger.Logger
+}
+
+type Request struct {
+	Metadata capabilities.RequestMetadata
+	Config   *values.Map
+	Inputs   sdk.CapMap
+}
+
+type capability struct {
+	lggr           logger.Logger
+	beholderClient *beholder.Client
+}
+
+func New(p Params) (capabilities.TargetCapability, error) {
+	beholderClient, err := newClientFn(beholder.DefaultConfig())
+	if err != nil {
+		return nil, err
+	}
+
+	return &capability{
+		lggr:           p.Logger,
+		beholderClient: beholderClient,
+	}, nil
+}
+
+func (c *capability) Info(_ context.Context) (capabilities.CapabilityInfo, error) {
+	return workflowEventTargetInfo, nil
+}
+
+func (c *capability) Execute(ctx context.Context, rawRequest capabilities.CapabilityRequest) (capabilities.CapabilityResponse, error) {
+	c.lggr.Debugw("executing", "workflowID", rawRequest.Metadata.WorkflowID, "executionID", rawRequest.Metadata.WorkflowExecutionID, "workflowName", rawRequest.Metadata.WorkflowName, "workflowOwner", rawRequest.Metadata.WorkflowOwner)
+
+	if rawRequest.Inputs == nil {
+		return capabilities.CapabilityResponse{}, errors.New("missing inputs field")
+	}
+
+	payload, ok := rawRequest.Inputs.Underlying["payload"]
+	if !ok || payload == nil {
+		return capabilities.CapabilityResponse{}, errors.New("missing payload")
+	}
+
+	payloadMap, ok := payload.(*values.Map)
+	if !ok {
+		return capabilities.CapabilityResponse{}, errors.New("payload is not a map")
+	}
+
+	pbMap := values.ProtoMap(payloadMap)
+
+	bytes, err := marshalFn(pbMap)
+	if err != nil {
+		return capabilities.CapabilityResponse{}, err
+	}
+
+	if err := c.beholderClient.Emitter.Emit(ctx, bytes,
+		"beholder_data_schema", "/custom-message/versions/1", // required
+		"beholder_data_type", "custom_message",
+		"beholder_domain", "platform",
+		"beholder_entity", "values",
+		"workflow_id", rawRequest.Metadata.WorkflowID,
+		"execution_id", rawRequest.Metadata.WorkflowExecutionID,
+		"workflow_name", rawRequest.Metadata.WorkflowName,
+		"workflow_owner", rawRequest.Metadata.WorkflowOwner,
+	); err != nil {
+		return capabilities.CapabilityResponse{}, err
+	}
+
+	c.lggr.Debugw("message emitted", "workflowID", rawRequest.Metadata.WorkflowID, "executionID", rawRequest.Metadata.WorkflowExecutionID, "workflowName", rawRequest.Metadata.WorkflowName, "workflowOwner", rawRequest.Metadata.WorkflowOwner)
+
+	return capabilities.CapabilityResponse{}, nil
+}
+
+func (c *capability) RegisterToWorkflow(_ context.Context, rawRequest capabilities.RegisterToWorkflowRequest) error {
+	c.lggr.Debugw("registering to workflow", "workflowID", rawRequest.Metadata.WorkflowID, "workflowOwner", rawRequest.Metadata.WorkflowOwner)
+	return nil
+}
+
+func (c *capability) UnregisterFromWorkflow(_ context.Context, rawRequest capabilities.UnregisterFromWorkflowRequest) error {
+	c.lggr.Debugw("unregistering from workflow", "workflowID", rawRequest.Metadata.WorkflowID, "workflowOwner", rawRequest.Metadata.WorkflowOwner)
+	return nil
+}
