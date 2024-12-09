630c9870a730a16cd0702796d19d16307d9ab436
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2024-12-09 09:35:09 +0000

    Add workflows

---
+++ workflows/fetchtrueusd/cmd/main.go
@@ -0,0 +1,160 @@
+package main
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/workflows/wasm"
+
+	"github.com/smartcontractkit/capabilities/cron/croncap"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/capabilities/consensus/ocr3/aggregators"
+	"github.com/smartcontractkit/chainlink-common/pkg/capabilities/consensus/ocr3/ocr3cap"
+	"github.com/smartcontractkit/chainlink-common/pkg/capabilities/targets/chainwriter"
+	"github.com/smartcontractkit/chainlink-common/pkg/workflows/sdk"
+)
+
+type trueUSDResponse struct {
+	AccountName string    `json:"accountName"`
+	TotalTrust  float64   `json:"totalTrust"`
+	Ripcord     bool      `json:"ripcord"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+}
+
+type computeOutput struct {
+	Price     int
+	FeedID    [32]byte
+	Timestamp int64
+}
+
+type computeConfig struct {
+	FeedID string
+}
+
+func convertFeedIDtoBytes(feedIDStr string) ([32]byte, error) {
+	b, err := hex.DecodeString(feedIDStr[2:])
+	if err != nil {
+		return [32]byte{}, err
+	}
+
+	if len(b) < 32 {
+		nb := [32]byte{}
+		copy(nb[:], b[:])
+		return nb, err
+	}
+
+	return [32]byte(b), nil
+}
+
+func BuildWorkflow(config []byte) *sdk.WorkflowSpecFactory {
+	workflow := sdk.NewWorkflowSpecFactory()
+
+	cron := croncap.Config{
+		Schedule: "*/30 * * * * *",
+	}.New(workflow)
+
+	compConf := computeConfig{
+		FeedID: "0x02afa5a69f0000220000000000000000",
+	}
+
+	compute := sdk.Compute1WithConfig(
+		workflow,
+		"compute",
+		&sdk.ComputeConfig[computeConfig]{Config: compConf},
+		sdk.Compute1Inputs[croncap.Payload]{Arg0: cron},
+		func(runtime sdk.Runtime, config computeConfig, outputs croncap.Payload) (computeOutput, error) {
+			feedID, err := convertFeedIDtoBytes(config.FeedID)
+			if err != nil {
+				return computeOutput{}, fmt.Errorf("cannot convert feedID to bytes")
+			}
+
+			fresp, err := runtime.Fetch(sdk.FetchRequest{
+				URL:       "https://api.real-time-reserves.verinumus.io/v1/chainlink/proof-of-reserves/TrueUSD",
+				Method:    "GET",
+				TimeoutMs: 5000,
+			})
+			if err != nil {
+				return computeOutput{}, err
+			}
+
+			var resp trueUSDResponse
+			err = json.Unmarshal(fresp.Body, &resp)
+			if err != nil {
+				return computeOutput{}, err
+			}
+
+			if resp.Ripcord {
+				err := runtime.Emitter().With(
+					"feedID", config.FeedID,
+				).Emit(fmt.Sprintf("ripcord flag set for feed ID %s", config.FeedID))
+				if err != nil {
+					runtime.Logger().Error("failed to emit custom message")
+				}
+				return computeOutput{}, sdk.BreakErr
+			}
+
+			return computeOutput{
+				Price:     int(resp.TotalTrust * 100),
+				FeedID:    feedID, // TrueUSD
+				Timestamp: resp.UpdatedAt.Unix(),
+			}, nil
+		},
+	)
+
+	consensusInput := ocr3cap.ReduceConsensusInput[computeOutput]{
+		Observation: compute.Value(),
+	}
+
+	consensus := ocr3cap.ReduceConsensusConfig[computeOutput]{
+		Encoder: ocr3cap.EncoderEVM,
+		EncoderConfig: map[string]any{
+			"abi": "(bytes32 FeedID, uint224 Price, uint32 Timestamp)[] Reports",
+		},
+		ReportID: "0001",
+		KeyID:    "evm",
+		AggregationConfig: aggregators.ReduceAggConfig{
+			Fields: []aggregators.AggregationField{
+				{
+					InputKey:  "FeedID",
+					OutputKey: "FeedID",
+					Method:    "mode",
+				},
+				{
+					InputKey:        "Price",
+					OutputKey:       "Price",
+					Method:          "median",
+					DeviationString: "1",
+					DeviationType:   "percent",
+				},
+				{
+					InputKey:        "Timestamp",
+					OutputKey:       "Timestamp",
+					Method:          "median",
+					DeviationString: "300",
+					DeviationType:   "absolute",
+				},
+			},
+			ReportFormat: aggregators.REPORT_FORMAT_ARRAY,
+		},
+	}.New(workflow, "consensus", consensusInput)
+
+	targetInput := chainwriter.TargetInput{
+		SignedReport: consensus,
+	}
+
+	chainwriter.TargetConfig{
+		Address:    "0xC4D5Af244E4Fe5e5f2D5a6b0F6F1867D4A5f0336", // Sepolia PoR Cache
+		DeltaStage: "15s",
+		Schedule:   "oneAtATime",
+	}.New(workflow, "write_ethereum-testnet-sepolia@1.0.0", targetInput)
+
+	return workflow
+}
+
+func main() {
+	runner := wasm.NewRunner()
+	workflow := BuildWorkflow(runner.Config())
+	runner.Run(workflow)
+}
---
+++ workflows/readbalances/cmd/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"encoding/hex"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"fmt"
+	"time"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/workflows/wasm"
+
+	"github.com/smartcontractkit/capabilities/cron/croncap"
+	"github.com/smartcontractkit/capabilities/readcontract/readcontractcap"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/capabilities/consensus/ocr3/aggregators"
+	"github.com/smartcontractkit/chainlink-common/pkg/capabilities/consensus/ocr3/ocr3cap"
+	"github.com/smartcontractkit/chainlink-common/pkg/capabilities/targets/chainwriter"
+	"github.com/smartcontractkit/chainlink-common/pkg/workflows/sdk"
+)
+
+type computeOutput struct {
+	Price     *big.Int
+	FeedID    [32]byte
+	Timestamp int64
+}
+
+type computeConfig struct {
+	FeedID string
+}
+
+func convertFeedIDtoBytes(feedIDStr string) ([32]byte, error) {
+	b, err := hex.DecodeString(feedIDStr[2:])
+	if err != nil {
+		return [32]byte{}, err
+	}
+
+	if len(b) < 32 {
+		nb := [32]byte{}
+		copy(nb[:], b[:])
+		return nb, err
+	}
+
+	return [32]byte(b), nil
+}
+
+func BuildWorkflow(config []byte) *sdk.WorkflowSpecFactory {
+	workflow := sdk.NewWorkflowSpecFactory()
+
+	cron := croncap.Config{
+		Schedule: "*/60 * * * * *", // Every 60 seconds
+	}.New(workflow)
+
+	addresses := []common.Address{
+		common.HexToAddress("0x5c25312C82791e6cB76Dc9eFaBE2F5fa695D966b"), // Keystone Dev Wallet #1
+		common.HexToAddress("0xAc85bE3811e06712f53BC11844Ed8a37d3e9C3Ab"), // Keystone Dev Wallet #2
+	}
+
+	// https://sepolia.etherscan.io/address/0x93c4bB995e7B5a726c8ef1bED9EA92e300F18eb4
+	balanceReaderContract := "0x93c4bB995e7B5a726c8ef1bED9EA92e300F18eb4"
+
+	chainRead := readcontractcap.Config{
+		ContractReaderConfig: `{"contracts":{"BalanceReader":{"contractABI":"[{\"inputs\":[{\"internalType\":\"address[]\",\"name\":\"addresses\",\"type\":\"address[]\"}],\"name\":\"getNativeBalances\",\"outputs\":[{\"internalType\":\"uint256[]\",\"name\":\"\",\"type\":\"uint256[]\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]","contractPollingFilter":{"genericEventNames":null,"pollingFilter":{"topic2":null,"topic3":null,"topic4":null,"retention":"0s","maxLogsKept":0,"logsPerBlock":0}},"configs":{"getNativeBalances":"{  \"chainSpecificName\": \"getNativeBalances\"}"}}}}`,
+		ContractAddress:      balanceReaderContract,
+		ContractName:         "BalanceReader",
+		ReadIdentifier:       fmt.Sprintf("%s-%s-%s", balanceReaderContract, "BalanceReader", "getNativeBalances"),
+	}.New(
+		workflow,
+		"read-contract-evm-11155111@1.0.0",
+		"read",
+		readcontractcap.ActionInput{
+			ConfidenceLevel: sdk.ConstantDefinition("finalized"),
+			Params: sdk.ConstantDefinition(readcontractcap.InputParams{
+				"addresses": addresses,
+				"_unused":   cron.Ref(), // TODO: Figure out a nicer way to do this.
+			}),
+		},
+	)
+	compConf := computeConfig{
+		FeedID: "0x02ce8bfc980000320000000000000000",
+	}
+
+	compute := sdk.Compute1WithConfig(
+		workflow,
+		"compute",
+		&sdk.ComputeConfig[computeConfig]{Config: compConf},
+		sdk.Compute1Inputs[readcontractcap.Output]{Arg0: chainRead},
+		func(runtime sdk.Runtime, config computeConfig, outputs readcontractcap.Output) (computeOutput, error) {
+			feedID, err := convertFeedIDtoBytes(config.FeedID)
+			if err != nil {
+				return computeOutput{}, fmt.Errorf("cannot convert feedID to bytes")
+			}
+
+			balances, ok := outputs.LatestValue.([]any)
+			if !ok {
+				return computeOutput{}, fmt.Errorf("cannot convert latest value to []*big.Int, got type %T", outputs.LatestValue)
+			}
+
+			balance := &big.Int{}
+			for _, bal := range balances {
+				bi, ok := bal.(*big.Int)
+				if !ok {
+					return computeOutput{}, fmt.Errorf("cannot convert value to *big.Int, got %T", bi)
+				}
+
+				balance = balance.Add(balance, bi)
+			}
+
+			return computeOutput{
+				Price:     balance,
+				FeedID:    feedID, // Randomly generated
+				Timestamp: time.Now().Unix(),
+			}, nil
+		},
+	)
+
+	consensusInput := ocr3cap.ReduceConsensusInput[computeOutput]{
+		Observation: compute.Value(),
+	}
+
+	consensus := ocr3cap.ReduceConsensusConfig[computeOutput]{
+		Encoder: ocr3cap.EncoderEVM,
+		EncoderConfig: map[string]any{
+			"abi": "(bytes32 FeedID, uint224 Price, uint32 Timestamp)[] Reports",
+		},
+		ReportID: "0001",
+		KeyID:    "evm",
+		AggregationConfig: aggregators.ReduceAggConfig{
+			Fields: []aggregators.AggregationField{
+				{
+					InputKey:  "FeedID",
+					OutputKey: "FeedID",
+					Method:    "mode",
+				},
+				{
+					InputKey:        "Price",
+					OutputKey:       "Price",
+					Method:          "median",
+					DeviationString: "1",
+					DeviationType:   "percent",
+				},
+				{
+					InputKey:        "Timestamp",
+					OutputKey:       "Timestamp",
+					Method:          "median",
+					DeviationString: "300", // 5 minutes
+					DeviationType:   "absolute",
+				},
+			},
+			ReportFormat: aggregators.REPORT_FORMAT_ARRAY,
+		},
+	}.New(workflow, "consensus", consensusInput)
+
+	targetInput := chainwriter.TargetInput{
+		SignedReport: consensus,
+	}
+
+	chainwriter.TargetConfig{
+		Address:    "0xC4D5Af244E4Fe5e5f2D5a6b0F6F1867D4A5f0336", // Sepolia PoR Cache
+		DeltaStage: "15s",
+		Schedule:   "onAtATime",
+	}.New(workflow, "write_ethereum-testnet-sepolia@1.0.0", targetInput)
+
+	return workflow
+}
+
+func main() {
+	runner := wasm.NewRunner()
+	workflow := BuildWorkflow(runner.Config())
+	runner.Run(workflow)
+}
