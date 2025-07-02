0b62d83c8fe0a2513878a33303a26b3a9020fc04
ilija <pavlovicilija42@gmail.com>
2025-07-02 15:56:38 +0200

    generate libs/monitoring

--- libs/monitoring/execution_context.pb.go
+++ libs/monitoring/execution_context.pb.go
@@ -7,12 +7,11 @@
 package monitoring
 
 import (
-	"reflect"
-	"sync"
-	"unsafe"
-
-	"google.golang.org/protobuf/reflect/protoreflect"
-	"google.golang.org/protobuf/runtime/protoimpl"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
+	unsafe "unsafe"
 )
 
 const (
@@ -195,7 +194,8 @@ var File_execution_context_proto protoreflect.FileDescriptor
 
 const file_execution_context_proto_rawDesc = "" +
 	"\n" +
-	"\x17execution_context.proto\x12\x06common\"\xc8\x06\n" +
+	"\x17execution_context.proto\x12\n" +
+	"monitoring\"\xc8\x06\n" +
 	"\x10ExecutionContext\x12$\n" +
 	"\x0emeta_source_id\x18\x01 \x01(\tR\fmetaSourceId\x123\n" +
 	"\x16meta_chain_family_name\x18\x02 \x01(\tR\x13metaChainFamilyName\x12\"\n" +
@@ -213,7 +213,7 @@ const file_execution_context_proto_rawDesc = "" +
 	"\x14meta_capability_type\x18\r \x01(\tR\x12metaCapabilityType\x12,\n" +
 	"\x12meta_capability_id\x18\x0e \x01(\tR\x10metaCapabilityId\x12E\n" +
 	"\x1fmeta_capability_timestamp_start\x18\x0f \x01(\x04R\x1cmetaCapabilityTimestampStart\x12C\n" +
-	"\x1emeta_capability_timestamp_emit\x18\x10 \x01(\x04R\x1bmetaCapabilityTimestampEmitB5Z3github.com/smartcontractkit/capabilities/libs/monitoringb\x06proto3"
+	"\x1emeta_capability_timestamp_emit\x18\x10 \x01(\x04R\x1bmetaCapabilityTimestampEmitB:Z8github.com/smartcontractkit/capabilities/libs/monitoringb\x06proto3"
 
 var (
 	file_execution_context_proto_rawDescOnce sync.Once
@@ -229,7 +229,7 @@ func file_execution_context_proto_rawDescGZIP() []byte {
 
 var file_execution_context_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
 var file_execution_context_proto_goTypes = []any{
-	(*ExecutionContext)(nil), // 0: common.ExecutionContext
+	(*ExecutionContext)(nil), // 0: monitoring.ExecutionContext
 }
 var file_execution_context_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
