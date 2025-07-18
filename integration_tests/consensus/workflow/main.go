e7b63365e53bbc0b08fb57daa5589975bbb54ce5
Matthew Pendrey <matthew.pendrey@gmail.com>
2025-07-18 12:46:38 +0100

    Updated the value consensus integration test to use latest workflow sdk (#177)
    * fix up consensus integration test
    
    * test
    
    * test
    
    * another flaky test

--- integration_tests/consensus/workflow/main.go
+++ integration_tests/consensus/workflow/main.go
@@ -5,57 +5,48 @@ package main
 import (
 	"fmt"
 
-	croncap "github.com/smartcontractkit/chainlink-common/pkg/capabilities/v2/triggers/cron"
-	"github.com/smartcontractkit/chainlink-common/pkg/workflows/sdk/v2"
-	"github.com/smartcontractkit/chainlink-common/pkg/workflows/wasm/v2"
+	"github.com/smartcontractkit/cre-sdk-go/capabilities/scheduler/cron"
+	"github.com/smartcontractkit/cre-sdk-go/sdk"
+	"github.com/smartcontractkit/cre-sdk-go/sdk/wasm"
 )
 
-func RunSimpleCronWorkflow(runner sdk.Runner[config]) {
-	cfg := &croncap.Config{
+func RunSimpleCronWorkflow(_ *sdk.Environment[struct{}]) (sdk.Workflow[struct{}], error) {
+	cfg := &cron.Config{
 		Schedule: "*/2 * * * * *", // every 2 seconds
 	}
 
-	runner.Run(func(env *sdk.Environment[config]) (sdk.Workflow[config], error) {
-		return sdk.Workflow[config]{
-			sdk.Handler(
-				croncap.Trigger(cfg),
-				onTrigger),
-		}, nil
-	})
+	return sdk.Workflow[struct{}]{
+		sdk.Handler(
+			cron.Trigger(cfg),
+			onTrigger,
+		),
+	}, nil
 }
 
-func onTrigger(env *sdk.Environment[config], runtime sdk.Runtime, outputs *croncap.Payload) (string, error) {
+func onTrigger(env *sdk.Environment[struct{}], runtime sdk.Runtime, outputs *cron.Payload) (string, error) {
 
 	var randomValue int64
 
-	promise := sdk.RunInNodeMode[config, int64](env,
-		runtime, func(env *sdk.NodeEnvironment[config], nrt sdk.NodeRuntime) (int64, error) {
-			nr, err := nrt.Rand()
-			if err != nil {
-				return 0, err
-			}
+	consensusValue, err := sdk.RunInNodeMode(env, runtime, func(env *sdk.NodeEnvironment[struct{}], nrt sdk.NodeRuntime) (int64, error) {
+		nr, err := nrt.Rand()
+		if err != nil {
+			return 0, err
+		}
 
-			randomValue = nr.Int63n(10)
+		randomValue = nr.Int63n(10)
 
-			return randomValue, nil
-		}, sdk.ConsensusMedianAggregation[int64]())
+		return randomValue, nil
+	}, sdk.ConsensusMedianAggregation[int64]()).Await()
 
-	consensusValue, err := promise.Await()
 	if err != nil {
-		return "", err
+		env.Logger.Error(fmt.Sprintf("Error in RunInNodeMode: %v", err))
+	} else {
+		env.Logger.Info(fmt.Sprintf("V2 Workflow Execution Result: trigger time %s local value %d, consensus value %d", outputs.ScheduledExecutionTime, randomValue, consensusValue))
 	}
-
-	env.Logger.Info(fmt.Sprintf("V2 Workflow Execution Result: trigger time %s local value %d, consensus value %d", outputs.ScheduledExecutionTime, randomValue, consensusValue))
 
 	return "complete", nil
 }
 
-type config struct {
-}
-
 func main() {
-	runner := wasm.NewRunner[config](func(configBytes []byte) (config, error) {
-		return config{}, nil
-	})
-	RunSimpleCronWorkflow(runner)
+	wasm.NewRunner(func(_ []byte) (struct{}, error) { return struct{}{}, nil }).Run(RunSimpleCronWorkflow)
 }
