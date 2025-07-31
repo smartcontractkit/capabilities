8a183c52f8a73e3461324d3fc5c3ebf49748bb8d
vreff <104409744+vreff@users.noreply.github.com>
2025-07-31 16:30:17 -0400

    Fix test flake (bandaid)

--- integration_tests/p2psigner/setup.go
+++ integration_tests/p2psigner/setup.go
@@ -2,6 +2,7 @@ package signertest
 
 import (
 	"context"
+	"sync"
 	"testing"
 	"time"
 
@@ -12,6 +13,8 @@ import (
 	"github.com/smartcontractkit/chainlink-common/pkg/services/servicetest"
 	"github.com/smartcontractkit/chainlink/v2/core/capabilities/integration_tests/framework"
 )
+
+const defaultTickInterval = 12 * time.Second
 
 func setupTestDon(ctx context.Context, t *testing.T, lggr logger.Logger,
 	workflowDonInfo framework.DonConfiguration, triggerSink framework.TriggerFactory, targetSink framework.TargetFactory, signerPath string) (workflowDon *framework.DON) {
@@ -29,9 +32,15 @@ func setupTestDon(ctx context.Context, t *testing.T, lggr logger.Logger,
 
 	workflowJob := GetWorkflowJob(t, workflowName, workflowOwnerID)
 
-	err := workflowDon.AddJob(ctx, &workflowJob)
-	require.NoError(t, err)
-
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		time.Sleep(defaultTickInterval)
+		err := workflowDon.AddJob(ctx, &workflowJob)
+		require.NoError(t, err)
+	}()
+	wg.Wait()
 	return workflowDon
 }
 
