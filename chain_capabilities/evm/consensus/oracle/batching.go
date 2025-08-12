9f37ae7d27924ca39350cfba6f40f9ede31c02a1
Dmytro Haidashenko <dmytro.haidashenko@smartcontract.com>
2025-08-12 13:10:25 +0200

    more linter fxies

--- chain_capabilities/evm/consensus/oracle/batching.go
+++ chain_capabilities/evm/consensus/oracle/batching.go
@@ -10,14 +10,14 @@ const (
 	mapValTagSize = 1 // field 2, tag size 2.
 )
 
-func sizeOfNewMapElement(mapKeyID int, key string, value proto.Message) int {
+func sizeOfNewMapElement(mapKeyID int32, key string, value proto.Message) int {
 	keySize := mapKeyTagSize + protowire.SizeBytes(len(key))
 	valSize := mapValTagSize + protowire.SizeBytes(proto.Size(value))
 	mapTagSize := protowire.SizeVarint(protowire.EncodeTag(protowire.Number(mapKeyID), protowire.BytesType)) // maps are tried as messages and presented on wire as Bytes
 	return mapTagSize + protowire.SizeBytes(keySize+valSize)
 }
 
-func hasCapacityToAdd(currentSize, mapKeyID int, key string, value proto.Message, maxSize int) (int, bool) {
+func hasCapacityToAdd(currentSize int, mapKeyID int32, key string, value proto.Message, maxSize int) (int, bool) {
 	newElementSize := sizeOfNewMapElement(mapKeyID, key, value)
 	return currentSize + newElementSize, currentSize+newElementSize <= maxSize
 }
