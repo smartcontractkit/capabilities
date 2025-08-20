7a1478897a64edb36b60d88c0b4ed6f5fda8d66a
Pablo La Greca <pablolagreca@hotmail.com>
2025-08-20 17:06:54 -0300

    merge
    refactoring name
    
    adding test

--- chain_capabilities/evm/monitoring/generate.go
+++ chain_capabilities/evm/monitoring/generate.go
@@ -1,3 +1,4 @@
 //go:generate go run ./gen/main.go
 //go:generate protoc --proto_path=../../../ --go_out=paths=source_relative:../../../ --go-grpc_out=paths=source_relative:../../../ chain_capabilities/evm/monitoring/read_actions.proto
+//go:generate protoc --proto_path=../../../ --go_out=paths=source_relative:../../../ --go-grpc_out=paths=source_relative:../../../ chain_capabilities/evm/monitoring/write_report.proto
 package monitoring
