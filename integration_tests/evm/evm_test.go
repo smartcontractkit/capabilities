eafeb6eb92742c605a948ac1a1956526f236d7f2
Lautaro Fernandez <juan.lautarofernandez@smartcontract.com>
2025-09-29 15:36:18 -0300

    PRODCRE-833: enable log trigger IT

--- integration_tests/evm/evm_test.go
+++ integration_tests/evm/evm_test.go
@@ -41,7 +41,7 @@ import (
 // It deploys a contract that emits logs, sets up a workflow with that deployed contract to the log trigger, waits for the workflow to be ready,
 // emits a log event, and then checks that the workflow processes the log event correctly by counting the number of events logged by beholder.
 func Test_LogTrigger(t *testing.T) {
-	t.Skip("PRODCRE-833 Needs to be fixed")
+	//	t.Skip("PRODCRE-833 Needs to be fixed")
 	ctx := t.Context()
 	beholderTester := beholdertest.NewObserver(t)
 	lggr, obs := logger.TestLoggerObserved(t, zapcore.InfoLevel)
@@ -121,6 +121,7 @@ func waitUntilLogPollerFiltersArePresent(t *testing.T, obs *observer.ObservedLog
 		return len(logs) == numOfWorkflowNodes
 	}, 2*time.Minute,
 		3*time.Second, "Expected to find %d log poller filters", numOfWorkflowNodes)
+	lggr.Infof("LogTrigger Filters are present, continue with the test")
 }
 
 type runtimeConfig struct {
