380ccf9ec3091093d674e4e11575318709b63a61
vreff <104409744+vreff@users.noreply.github.com>
2025-09-30 17:32:14 -0400

    updates

--- consensus/main.go
+++ consensus/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/jonboulle/clockwork"
 
 	"github.com/smartcontractkit/chainlink-common/pkg/capabilities/v2/consensus/server"
-	"github.com/smartcontractkit/chainlink-common/pkg/logger"
 	"github.com/smartcontractkit/chainlink-common/pkg/loop"
 
 	"github.com/smartcontractkit/capabilities/consensus/action"
@@ -15,10 +14,10 @@ import (
 )
 
 func main() {
-	loopserver.Serve("ConsensusCapability", func(lggr logger.Logger) loop.StandardCapabilities {
-		capability, err := action.NewConsensusCapability(lggr, clockwork.NewRealClock(), 1*time.Minute)
+	loopserver.ServeNew("ConsensusCapability", func(s *loop.Server) loop.StandardCapabilities {
+		capability, err := action.NewConsensusCapability(s.Logger, clockwork.NewRealClock(), 1*time.Minute)
 		if err != nil {
-			lggr.Fatalw("Failed to create ConsensusCapability", "error", err)
+			s.Logger.Fatalw("Failed to create ConsensusCapability", "error", err)
 		}
 
 		return server.NewConsensusServer(capability)
