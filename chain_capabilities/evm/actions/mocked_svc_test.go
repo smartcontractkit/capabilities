0fd9a37675210b80b65995f733819ba8e1f69f48
ilija <pavlovicilija42@gmail.com>
2025-10-08 15:51:00 +0200

    fix init test mocks

--- chain_capabilities/evm/actions/mocked_svc_test.go
+++ chain_capabilities/evm/actions/mocked_svc_test.go
@@ -26,7 +26,8 @@ func InitMocks(t *testing.T) *EvmWithMocks {
 	evmSvc := evmmock.NewEVMService(t)
 	consensusHandler := mocks.NewConsensusHandler(t)
 	lggr := logger.Test(t)
-	evm, err := NewEVM(config.Config{}, evmSvc, lggr, test.NopBeholderProcessor{}, &monitoring.MessageBuilder{}, consensusHandler, 1, limits.Factory{Logger: lggr})
+	randomEVMAddress := "0xFc5df03D4E91bae4c118B7dda995476f332C9d8C"
+	evm, err := NewEVM(config.Config{CREForwarderAddress: randomEVMAddress}, evmSvc, lggr, test.NopBeholderProcessor{}, &monitoring.MessageBuilder{}, consensusHandler, 1, limits.Factory{Logger: lggr})
 	require.NoError(t, err)
 	return &EvmWithMocks{
 		EVM:              evm,
