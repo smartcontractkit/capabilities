224720229a51c6c8e328842cc3934c24a018b832
DeividasK <deividas.karzinauskas@gmail.com>
2025-10-29 10:36:51 +0200

    Change common errors file to see if it helps include the file

--- http_action/common/errors.go
+++ http_action/common/errors.go
@@ -4,5 +4,5 @@ const (
 	ErrorOutgoingRatelimitGlobal        = "global limit of outgoing gateways requests has been exceeded"
 	ErrorOutgoingRatelimitWorkflowOwner = "workflow owner exceeded limit of gateways requests"
 	ErrorIncomingRatelimitGlobal        = "message from gateway exceeded global rate limit"
-	ErrorIncomingRatelimitSender        = "message from gateway exceeded per sender rate limit"
+	ErrorIncomingRatelimitSender        = "message from gateway exceeded per sender rate limits"
 )
