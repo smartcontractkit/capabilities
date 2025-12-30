3e365241036963c1df899abfeb5cfe79f0a62f1b
Juan Lautaro Fernandez <juan.lautarofernandez@smartcontract.com>
2025-12-30 10:38:54 -0300

    PLEX-2346: adding permutations of ITs for evm logTrigger (#388)

--- integration_tests/evm/contract/MessageEmitter.sol
+++ integration_tests/evm/contract/MessageEmitter.sol
@@ -4,8 +4,25 @@ pragma solidity ^0.8.0;
 contract MessageEmitter {
     event MessageEmitted(string message);
 
+    // New event with multiple indexed parameters for multi-topic log trigger tests
+    event MultiTopicEmitted(
+        uint256 indexed topic2,
+        uint256 indexed topic3,
+        uint256 indexed topic4,
+        string message
+    );
+
     function emitMessage(string calldata message) external {
         emit MessageEmitted(message);
     }
-}
 
+    // Emits the MultiTopicEmitted event so tests can exercise topic2-4 filtering
+    function emitMultiTopic(
+        uint256 topic2,
+        uint256 topic3,
+        uint256 topic4,
+        string calldata message
+    ) external {
+        emit MultiTopicEmitted( topic2, topic3, topic4, message);
+    }
+}
