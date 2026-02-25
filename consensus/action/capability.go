881f3d3863f670af2c2500f5e2175f08fe4002d6
cawthorne <cawthornegd@gmail.com>
2026-02-25 13:52:17 +0000

    Add ed25519 + blake2b_256 signing schme

--- consensus/action/capability.go
+++ consensus/action/capability.go
@@ -50,7 +50,9 @@ const (
 	KeyBundleIDAptos                    = "aptos"
 	KeyBundleIDSolana                   = "solana"
 	SigningAlgoEcdsa                    = "ecdsa"
+	SigningAlgoEd25519                  = "ed25519"
 	HashingAlgoKeccak256                = "keccak256"
+	HashingAlgoBlake2b256               = "blake2b_256"
 )
 
 type ConsensusCapabilityConfig struct {
@@ -422,7 +424,8 @@ func validateReportRequest(reportRequest *sdk.ReportRequest) (string, error) {
 
 	signingAlgo := strings.ToLower(reportRequest.SigningAlgo)
 	supportedSigningAlgorithms := map[string]struct{}{
-		SigningAlgoEcdsa: {},
+		SigningAlgoEcdsa:   {},
+		SigningAlgoEd25519: {},
 	}
 
 	if _, ok := supportedSigningAlgorithms[signingAlgo]; !ok {
@@ -432,7 +435,8 @@ func validateReportRequest(reportRequest *sdk.ReportRequest) (string, error) {
 	hashingAlgo := strings.ToLower(reportRequest.HashingAlgo)
 
 	supportedHashingAlgorithms := map[string]struct{}{
-		HashingAlgoKeccak256: {},
+		HashingAlgoKeccak256:  {},
+		HashingAlgoBlake2b256: {},
 	}
 
 	if _, ok := supportedHashingAlgorithms[hashingAlgo]; !ok {
