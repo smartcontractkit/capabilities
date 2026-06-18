c6171810934f2e89a631ed653e4d109fa48d116f
yashnevatia <yashvardhan.nevatia@smartcontract.com>
2026-06-18 14:14:50 +0100

    use factory logger

--- chain_capabilities/aptos/actions/actions.go
+++ chain_capabilities/aptos/actions/actions.go
@@ -71,10 +71,10 @@ func NewAptos(cfg *config.Config, p2pConfig map[string]string, aptosService type
 		messageBuilder:         messageBuilder,
 	}
 
-	return a, a.initLimiters(limitsFactory, lggr)
+	return a, a.initLimiters(limitsFactory)
 }
 
-func (a *Aptos) initLimiters(limitsFactory limits.Factory, lggr logger.Logger) (err error) {
+func (a *Aptos) initLimiters(limitsFactory limits.Factory) (err error) {
 	a.reportSizeLimit, err = limits.MakeUpperBoundLimiter(limitsFactory, cresettings.Default.PerWorkflow.ChainWrite.Aptos.ReportSizeLimit)
 	if err != nil {
 		return
@@ -84,7 +84,7 @@ func (a *Aptos) initLimiters(limitsFactory limits.Factory, lggr logger.Logger) (
 	if err != nil {
 		return
 	}
-	lggr.Debugw("Initializing write report block timestamp active period limiter", "activePeriod", cresettings.Default.PerWorkflow.FeatureAptosWriteReportBlockTimestampActivePeriod)
+	limitsFactory.Logger.Debugw("Initializing write report block timestamp active period limiter", "activePeriod", cresettings.Default.PerWorkflow.FeatureAptosWriteReportBlockTimestampActivePeriod)
 	a.writeReportBlockTimestampActive, err = limits.MakeRangeLimiter(limitsFactory, cresettings.Default.PerWorkflow.FeatureAptosWriteReportBlockTimestampActivePeriod)
 	if err != nil {
 		return
