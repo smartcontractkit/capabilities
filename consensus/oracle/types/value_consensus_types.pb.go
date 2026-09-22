034a75642264d7936375e43dbf6a796f5ca2a999
Cedric Cordenier <cedric.cordenier@smartcontract.com>
2026-09-22 16:01:32 +0100

    Address review comments

--- consensus/oracle/types/value_consensus_types.pb.go
+++ consensus/oracle/types/value_consensus_types.pb.go
@@ -317,12 +317,14 @@ func (x *Query) GetRequestIDs() []string {
 }
 
 type RequestObservation struct {
-	state         protoimpl.MessageState     `protogen:"open.v1"`
-	Metadata      *RequestMetaData           `protobuf:"bytes,1,opt,name=metadata,proto3" json:"metadata,omitempty"`
-	Input         *sdk.SimpleConsensusInputs `protobuf:"bytes,2,opt,name=input,proto3" json:"input,omitempty"`
-	ReceivedAt    *timestamppb.Timestamp     `protobuf:"bytes,3,opt,name=received_at,json=receivedAt,proto3" json:"received_at,omitempty"`
-	unknownFields protoimpl.UnknownFields
-	sizeCache     protoimpl.SizeCache
+	state                                      protoimpl.MessageState     `protogen:"open.v1"`
+	Metadata                                   *RequestMetaData           `protobuf:"bytes,1,opt,name=metadata,proto3" json:"metadata,omitempty"`
+	Input                                      *sdk.SimpleConsensusInputs `protobuf:"bytes,2,opt,name=input,proto3" json:"input,omitempty"`
+	ReceivedAt                                 *timestamppb.Timestamp     `protobuf:"bytes,3,opt,name=received_at,json=receivedAt,proto3" json:"received_at,omitempty"`
+	RemoveLibUseInFailureMessageFormattingFlag bool                       `protobuf:"varint,4,opt,name=remove_lib_use_in_failure_message_formatting_flag,json=removeLibUseInFailureMessageFormattingFlag,proto3" json:"remove_lib_use_in_failure_message_formatting_flag,omitempty"` // remove use of libraries in failure message formatting; deprecated/not used.
+	UpdateErrorHandlingFlag                    bool                       `protobuf:"varint,5,opt,name=update_error_handling_flag,json=updateErrorHandlingFlag,proto3" json:"update_error_handling_flag,omitempty"`                                                                  // migrate system errors to user errors; deprecated/not used
+	unknownFields                              protoimpl.UnknownFields
+	sizeCache                                  protoimpl.SizeCache
 }
 
 func (x *RequestObservation) Reset() {
@@ -374,6 +376,20 @@ func (x *RequestObservation) GetReceivedAt() *timestamppb.Timestamp {
 		return x.ReceivedAt
 	}
 	return nil
+}
+
+func (x *RequestObservation) GetRemoveLibUseInFailureMessageFormattingFlag() bool {
+	if x != nil {
+		return x.RemoveLibUseInFailureMessageFormattingFlag
+	}
+	return false
+}
+
+func (x *RequestObservation) GetUpdateErrorHandlingFlag() bool {
+	if x != nil {
+		return x.UpdateErrorHandlingFlag
+	}
+	return false
 }
 
 type Observation struct {
@@ -812,12 +828,14 @@ const file_value_consensus_types_proto_rawDesc = "" +
 	"\x05Query\x12\x1e\n" +
 	"\n" +
 	"requestIDs\x18\x01 \x03(\tR\n" +
-	"requestIDs\"\xdb\x01\n" +
+	"requestIDs\"\xf3\x02\n" +
 	"\x12RequestObservation\x12B\n" +
 	"\bmetadata\x18\x01 \x01(\v2&.value_consensus_types.RequestMetaDataR\bmetadata\x128\n" +
 	"\x05input\x18\x02 \x01(\v2\".sdk.v1alpha.SimpleConsensusInputsR\x05input\x12;\n" +
 	"\vreceived_at\x18\x03 \x01(\v2\x1a.google.protobuf.TimestampR\n" +
-	"receivedAtJ\x04\b\x04\x10\x05J\x04\b\x05\x10\x06\"\xd3\x01\n" +
+	"receivedAt\x12e\n" +
+	"1remove_lib_use_in_failure_message_formatting_flag\x18\x04 \x01(\bR*removeLibUseInFailureMessageFormattingFlag\x12;\n" +
+	"\x1aupdate_error_handling_flag\x18\x05 \x01(\bR\x17updateErrorHandlingFlag\"\xd3\x01\n" +
 	"\vObservation\x12X\n" +
 	"\fobservations\x18\x01 \x03(\v24.value_consensus_types.Observation.ObservationsEntryR\fobservations\x1aj\n" +
 	"\x11ObservationsEntry\x12\x10\n" +
