7ec39bcd4d12835641393a28cba77788dd1be2cf
ilija42 <pavlovicilija42@gmail.com>
2026-09-23 17:32:29 +0200

    libs/monitoring: add chain-agnostic WriteReportTransactions message
    Add a reusable monitoring.WriteReportTransactions proto (repeated tx_hashes +
    ExecutionContext) that any chain capability can emit to report the set of
    on-chain transactions produced by a WriteReport call, keyed by the workflow /
    execution identity in ExecutionContext.
    
    Includes a Go helper implementing the common Message interface (LogAttributes /
    MetricAttributes delegate to ExecutionContext; tx hashes excluded from metric
    labels to avoid high cardinality).
    
    execution_context.pb.go regenerated alongside so both files share a consistent
    init symbol.

--- libs/monitoring/execution_context.pb.go
+++ libs/monitoring/execution_context.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // 	protoc-gen-go v1.36.10
 // 	protoc        v5.29.3
-// source: execution_context.proto
+// source: libs/monitoring/execution_context.proto
 
 package monitoring
 
@@ -50,7 +50,7 @@ type ExecutionContext struct {
 
 func (x *ExecutionContext) Reset() {
 	*x = ExecutionContext{}
-	mi := &file_execution_context_proto_msgTypes[0]
+	mi := &file_libs_monitoring_execution_context_proto_msgTypes[0]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -62,7 +62,7 @@ func (x *ExecutionContext) String() string {
 func (*ExecutionContext) ProtoMessage() {}
 
 func (x *ExecutionContext) ProtoReflect() protoreflect.Message {
-	mi := &file_execution_context_proto_msgTypes[0]
+	mi := &file_libs_monitoring_execution_context_proto_msgTypes[0]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -75,7 +75,7 @@ func (x *ExecutionContext) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use ExecutionContext.ProtoReflect.Descriptor instead.
 func (*ExecutionContext) Descriptor() ([]byte, []int) {
-	return file_execution_context_proto_rawDescGZIP(), []int{0}
+	return file_libs_monitoring_execution_context_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *ExecutionContext) GetMetaSourceId() string {
@@ -190,11 +190,11 @@ func (x *ExecutionContext) GetMetaCapabilityTimestampEmit() uint64 {
 	return 0
 }
 
-var File_execution_context_proto protoreflect.FileDescriptor
+var File_libs_monitoring_execution_context_proto protoreflect.FileDescriptor
 
-const file_execution_context_proto_rawDesc = "" +
+const file_libs_monitoring_execution_context_proto_rawDesc = "" +
 	"\n" +
-	"\x17execution_context.proto\x12\n" +
+	"'libs/monitoring/execution_context.proto\x12\n" +
 	"monitoring\"\xc8\x06\n" +
 	"\x10ExecutionContext\x12$\n" +
 	"\x0emeta_source_id\x18\x01 \x01(\tR\fmetaSourceId\x123\n" +
@@ -216,22 +216,22 @@ const file_execution_context_proto_rawDesc = "" +
 	"\x1emeta_capability_timestamp_emit\x18\x10 \x01(\x04R\x1bmetaCapabilityTimestampEmitB:Z8github.com/smartcontractkit/capabilities/libs/monitoringb\x06proto3"
 
 var (
-	file_execution_context_proto_rawDescOnce sync.Once
-	file_execution_context_proto_rawDescData []byte
+	file_libs_monitoring_execution_context_proto_rawDescOnce sync.Once
+	file_libs_monitoring_execution_context_proto_rawDescData []byte
 )
 
-func file_execution_context_proto_rawDescGZIP() []byte {
-	file_execution_context_proto_rawDescOnce.Do(func() {
-		file_execution_context_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_execution_context_proto_rawDesc), len(file_execution_context_proto_rawDesc)))
+func file_libs_monitoring_execution_context_proto_rawDescGZIP() []byte {
+	file_libs_monitoring_execution_context_proto_rawDescOnce.Do(func() {
+		file_libs_monitoring_execution_context_proto_rawDescData = protoimpl.X.CompressGZIP(unsafe.Slice(unsafe.StringData(file_libs_monitoring_execution_context_proto_rawDesc), len(file_libs_monitoring_execution_context_proto_rawDesc)))
 	})
-	return file_execution_context_proto_rawDescData
+	return file_libs_monitoring_execution_context_proto_rawDescData
 }
 
-var file_execution_context_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_execution_context_proto_goTypes = []any{
+var file_libs_monitoring_execution_context_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_libs_monitoring_execution_context_proto_goTypes = []any{
 	(*ExecutionContext)(nil), // 0: monitoring.ExecutionContext
 }
-var file_execution_context_proto_depIdxs = []int32{
+var file_libs_monitoring_execution_context_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -239,26 +239,26 @@ var file_execution_context_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_execution_context_proto_init() }
-func file_execution_context_proto_init() {
-	if File_execution_context_proto != nil {
+func init() { file_libs_monitoring_execution_context_proto_init() }
+func file_libs_monitoring_execution_context_proto_init() {
+	if File_libs_monitoring_execution_context_proto != nil {
 		return
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: unsafe.Slice(unsafe.StringData(file_execution_context_proto_rawDesc), len(file_execution_context_proto_rawDesc)),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_libs_monitoring_execution_context_proto_rawDesc), len(file_libs_monitoring_execution_context_proto_rawDesc)),
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_execution_context_proto_goTypes,
-		DependencyIndexes: file_execution_context_proto_depIdxs,
-		MessageInfos:      file_execution_context_proto_msgTypes,
+		GoTypes:           file_libs_monitoring_execution_context_proto_goTypes,
+		DependencyIndexes: file_libs_monitoring_execution_context_proto_depIdxs,
+		MessageInfos:      file_libs_monitoring_execution_context_proto_msgTypes,
 	}.Build()
-	File_execution_context_proto = out.File
-	file_execution_context_proto_goTypes = nil
-	file_execution_context_proto_depIdxs = nil
+	File_libs_monitoring_execution_context_proto = out.File
+	file_libs_monitoring_execution_context_proto_goTypes = nil
+	file_libs_monitoring_execution_context_proto_depIdxs = nil
 }
